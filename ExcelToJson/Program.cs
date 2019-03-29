9783db675017e95006391581af6b9886eb666eea
Nidhish PS <nidps@users.noreply.github.com>
2019-03-29 23:35:53 +0530

    Added function to converts excel to json using ExcelDataReader

--- ExcelToJson/Program.cs
+++ ExcelToJson/Program.cs
@@ -3,6 +3,8 @@ using System.Data.Common;
 using System.Data.OleDb;
 using System.IO;
 using System.Linq;
+using System.Text;
+using ExcelDataReader;
 using Newtonsoft.Json;
 
 namespace ExcelToJson
@@ -14,7 +16,11 @@ namespace ExcelToJson
             var inFilePath = args[0];
             var outFilePath = args[1];
             var sheetName = args[2];
+            UsingExcelDataReader(inFilePath, outFilePath);
+        }
 
+        private static void UsingOleDb(string inFilePath, string outFilePath, string sheetName)
+        {
             var connectionString = $@"
                 Provider=Microsoft.ACE.OLEDB.12.0;
                 Data Source={inFilePath};
@@ -44,6 +50,48 @@ namespace ExcelToJson
                     var json = JsonConvert.SerializeObject(query);
                     File.WriteAllText(outFilePath, json);
                 }
+            }
+        }
+
+        private static void UsingExcelDataReader(string inputFile, string outputFile)
+        {
+            using (var inFile = File.Open(inputFile, FileMode.Open, FileAccess.Read))
+            using (var outFile = File.CreateText(outputFile))
+            using (var reader = ExcelReaderFactory.CreateReader(inFile,
+                new ExcelReaderConfiguration {FallbackEncoding = Encoding.GetEncoding(1252)}))
+            using (var writer = new JsonTextWriter(outFile))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.WriteStartArray();
+                //SKIP FIRST ROW, it's TITLES.
+                reader.Read();
+                do
+                {
+                    while (reader.Read())
+                    {
+                        //We don't need empty object
+                        var firstName = reader.GetString(0);
+                        if (string.IsNullOrEmpty(firstName)) break;
+
+                        writer.WriteStartObject();
+                        //Select Columns and values
+                        writer.WritePropertyName("FirstName");
+                        writer.WriteValue(firstName);
+
+                        writer.WritePropertyName("LastName");
+                        writer.WriteValue(reader.GetString(1));
+
+                        writer.WritePropertyName("Gender");
+                        writer.WriteValue(reader.GetString(2));
+
+                        writer.WritePropertyName("State");
+                        writer.WriteValue(reader.GetString(3));
+
+                        writer.WriteEndObject();
+                    }
+                } while (reader.NextResult());
+
+                writer.WriteEndArray();
             }
         }
     }
